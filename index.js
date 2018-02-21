a5e9fabfb3a0bce93d138fb1dfc18d5f00d6e0f1
Marco Massarotto <marco.massarotto@lloydsbanking.com>
2018-02-21 08:47:23 +0000

    feat: upgrade to semantic-release 15
    BREAKING CHANGE: requires semantic-release ^15.0.0 as peerDependency

--- index.js
+++ index.js
@@ -1,63 +1,29 @@
+const releaseRules = require('./releaseRules');
+
 module.exports = {
   branch: 'master',
-  verifyConditions: [
-    '@semantic-release/git',
-    '@semantic-release/gitlab'
-  ],
   generateNotes: {
     config: '@cobnl/conventional-changelog'
   },
   analyzeCommits: {
-    releaseRules: [
-      {
-        breaking: true,
-        release: 'major'
-      },
-      {
-        type: 'feat',
-        release: 'minor'
-      },
-      {
-        type: 'fix',
-        release: 'patch'
-      },
-      {
-        type: 'docs',
-        release: 'patch'
-      },
-      {
-        type: 'style',
-        release: 'patch'
-      },
-      {
-        type: 'refactor',
-        release: 'patch'
-      },
-      {
-        type: 'perf',
-        release: 'patch'
-      },
-      {
-        type: 'test',
-        release: 'patch'
-      },
-      {
-        type: 'chore',
-        release: 'patch'
-      }
-    ]
+    releaseRules
   },
-  publish: [
+  verifyConditions: [
+    '@semantic-release/git',
+    '@semantic-release/gitlab'
+  ],
+  prepare: [
     {
       path: '@semantic-release/npm',
-      npmPublish: false,
       tarballDir: 'artifacts'
     },
     {
       // eslint-disable-next-line no-template-curly-in-string
       message: 'chore: release ${nextRelease.version}',
       path: '@semantic-release/git'
-    },
+    }
+  ],
+  publish: [
     '@semantic-release/gitlab'
   ],
   success: false,
---
+++ releaseRules.js
@@ -0,0 +1,38 @@
+module.exports = [
+  {
+    breaking: true,
+    release: 'major'
+  },
+  {
+    type: 'feat',
+    release: 'minor'
+  },
+  {
+    type: 'fix',
+    release: 'patch'
+  },
+  {
+    type: 'docs',
+    release: 'patch'
+  },
+  {
+    type: 'style',
+    release: 'patch'
+  },
+  {
+    type: 'refactor',
+    release: 'patch'
+  },
+  {
+    type: 'perf',
+    release: 'patch'
+  },
+  {
+    type: 'test',
+    release: 'patch'
+  },
+  {
+    type: 'chore',
+    release: 'patch'
+  }
+];
